cb3cf986a3784a4a7f127e66aba190e13999e9d2
jesushenriquez09 <jesuselchepo@gmail.com>
2025-10-19 14:58:33 -0500

    nlp

---
+++ routes/palabras_claves.py
@@ -0,0 +1,28 @@
+import nltk 
+from nltk.probability import FreqDist 
+from nltk.tokenize  import word_tokenize
+from nltk.corpus import stopwords
+
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
+texto = """ 
+Tengo dolor al orinar y molestia en la vejiga desde hace dos días.
+""" 
+
+
+def nlp_palabras_claves(texto: str):
+    tokens = word_tokenize(texto.lower())
+    try:
+     stop_words = set(stopwords.words('spanish'))
+    except:
+     print("No se pudieron cargar las stopwords en español. Asegúrese de tener el paquete de stopwords descargado.")
+    strop_words = set() 
+    
+    tokens_filtrados = [word for word in tokens if word.isalnum() and word not in stop_words]   
+    frecuencia = FreqDist(tokens_filtrados)
+    print("palabras claves")
+    for palabra,frecuencia in frecuencia.most_common(5):
+     print(f"{palabra}: {frecuencia}")
+
+nlp_palabras_claves(texto) 
